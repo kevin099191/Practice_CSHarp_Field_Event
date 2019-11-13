9995af45483ad52e71a049ce7bbfd034d5b539ce
kevin099191 <57181260+kevin099191@users.noreply.github.com>
2019-11-13 19:27:04 +0800

    打上程式

---
+++ UnityProject/Assets/Chikth.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+
+public class Chikth : MonoBehaviour
+{
+
+    [Header("移動速度"),Range(0.1f,99.9f)]
+    public float Speed;
+    [Header("跳躍高度"), Range(0, 2000)]
+    public int Hight;
+    [Header("對話內容")]
+    public string Talk = "咕咕咕~~~";
+    [Header("是否取得雞蛋")]
+    public bool egg;
+
+    private void Start()
+    {
+        print("遊戲開始");
+        print("咕咕咕~~~");
+    }
+
+
+
+
+
+
+}
